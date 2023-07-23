5b7f87ada096588343105ca3a2325548050e650e
SpehtOlena <speht_olena@ukr.net>
2023-07-23 12:13:02 +0300

    main components

--- app_1/src/App.js
+++ app_1/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import './style/reset.css';
+import Page from './components/Page/Page';
 
 function App() {
 	return (
 		<div className="App">
-
+			<Page />
 		</div >
 	);
 }
---
+++ app_1/src/components/Page/Page.js
@@ -0,0 +1,11 @@
+import Button from "../Button/Button.js";
+
+
+const Page = () => {
+	return (
+		<div>
+
+		</div>
+	)
+}
+export default Page
