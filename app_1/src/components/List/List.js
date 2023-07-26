75a5c62c02b177237599be3717546c65da9c95e0
SpehtOlena <speht_olena@ukr.net>
2023-07-26 19:03:25 +0300

    correction of display of ingredients with map

--- app_1/src/components/List/List.js
+++ app_1/src/components/List/List.js
@@ -1,16 +1,45 @@
-import ListCard from "./ListCard";
 import './List.css';
 import recipesLink from "./ListArray";
+import Button from '../Button/Button';
+import Modal from '../Modal/Modal';
+import { useState } from 'react';
+import Search from '../Search/Search';
 
 
-const List = () => {
+
+const List = ({ item }) => {
+	const [modalOpen, setModalOpen] = useState(false);
+	const closeModal = () => {
+		setModalOpen(false)
+	}
 	return (
 		<div className="ListWrapper">
-			<div className="List_container">
+			<ul className="List_container">
 				{
-					recipesLink.map(value => <ListCard item={value} key={value.ingredients} />)
+					recipesLink.map(item => (
+						<li key={item.id} className='dishItem'>
+							<img className='dishImage' src={item.img} alt={`${item.name} photo`} />
+							<h3 className='dishName'>{item.name}</h3>
+							<ul className='dishingredients'>
+								{
+									item.ingredients.map((subItem, index) => (
+										<li key={index}>
+											{subItem}
+										</li>
+									))
+								}
+							</ul>
+							<Button onClick={() => setModalOpen(true)} primary={true}>Show detailss</Button>
+							<Modal isOpen={modalOpen} onClose={closeModal}>
+								<Search />
+
+							</Modal>
+						</li>
+					))
 				}
-			</div>
+
+			</ul>
+
 		</div>
 
 	)
