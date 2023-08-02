51f534d2d43d3f81e7721023f880e3ae7463cf93
SpehtOlena <speht_olena@ukr.net>
2023-08-02 22:23:20 +0300

    empty app

--- app_1/src/App.js
+++ app_1/src/App.js
@@ -1,14 +1,8 @@
-import { useState } from 'react';
 import './App.css';
-import AddUsers from './components/AddUsers/AddUsers';
 
 function App() {
-	const [activeUser, setActiveUser] = useState(null)
 	return (
 		<div className="App">
-			{!!activeUser ? <div>
-				{activeUser.name}
-			</div> : <AddUsers />}
 
 		</div >
 	);
